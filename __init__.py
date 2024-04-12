ec1fc0263de9f05cf213d6b82d3a13dccc271d11
lara-hcf <150332229+lara-hcf@users.noreply.github.com>
2024-04-12 12:20:31 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -49,7 +49,7 @@ def Readfiche(post_id):
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
 
-@app.route('/fiche_client/<string:nom_client>')
+@app.route('/fiche_client/<string:nom_client>', methods=['POST'])
 def afficheClient(nom_client):
     
     conn= sqlite3.connect('database.db')
@@ -57,11 +57,14 @@ def afficheClient(nom_client):
     cursor.execute('SELECT * FROM clients WHERE nom = ?', (nom_client,))
     data= cursor.fetchall()
     conn.close()
-    if request.form['username']== 'user' and request.form['password']== '123':
-        session['authentifie']= True
-        return render_template('read_data.html', data=data)
+    if request.method== 'POST':
+        if request.form['username']== 'user' and request.form['password']== '123':
+            session['authentifie']= True
+            return render_template('read_data.html', data=data)
+        else:
+            return "<h1>Erreur d'identification</h1>"
     else:
-        return "<h1>Erreur</h1>"
+        return "<h1>Erreur 404</h2>"
 
 @app.route('/consultation/')
 def ReadBDD():
