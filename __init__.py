f155b65228095f3b54390a5c788aafcf54c7751b
lara-hcf <150332229+lara-hcf@users.noreply.github.com>
2024-04-12 11:56:03 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -49,7 +49,7 @@ def Readfiche(post_id):
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
 
-@app.route('/fiche_client/<string: nom_client>')
+@app.route('/fiche_client/<string:nom_client>')
 def afficheClient(nom_client):
     conn= sqlite3.connect('database.db')
     cursor= conn.cursor()
