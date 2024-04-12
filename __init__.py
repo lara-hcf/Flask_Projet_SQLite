31361f4b8ae207a077229dbb4f129983d7d4853c
lara-hcf <150332229+lara-hcf@users.noreply.github.com>
2024-04-12 13:18:37 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -60,7 +60,7 @@ def afficheClient(nom_client):
     data= cursor.fetchall()
     conn.close()
     if not est_authentifie():
-        return redirect(url_for('authentification'))
+        return redirect(url_for('authentification')), nom_client=nom_client
     else : 
         return render_template('read_data.html', data=data)
 
